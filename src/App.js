3b2f4f8e1aee7e1f238a91d7619b0eaf5941ef0e
JiwonChoi0805 <won0805@ewhain.net>
2023-11-30 10:39:01 +0900

    style: comment components style + 공용색상 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import GlobalStyle from "./statics/styles/GlobalStyle";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import MainPage from "./pages/Mainpage";
+import Detailpage from "./pages/Detailpage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path={"/"} element={<MainPage />}></Route>
+          <Route path={"/detail"} element={<Detailpage />}></Route>
         </Routes>
       </BrowserRouter>
     </>
