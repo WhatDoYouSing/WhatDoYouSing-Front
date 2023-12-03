0550dd05f5bff79fd6192e90665364f4fbf27a7a
mod_siw <pvq_71@ewhain.net>
2023-12-03 22:50:13 +0900

    ??? : scroll-snap으로 추천 페이지 구현해보려다가 실패 ( 방법 찾아야 함 )

--- src/pages/RecommendPage.jsx
+++ src/pages/RecommendPage.jsx
@@ -3,17 +3,43 @@ import styled from "styled-components";
 
 //components
 import LyricsItem from "../components/common/LyricsItem";
-import EmotionList from "../components/common/EmotionList";
-import DropDownBox from "../components/common/DropDownBox";
 
 import Topbar from "../components/common/MainPage/Topbar";
 
 const RecommendPage = () => {
+  const chartItems = Array.from({ length: 3 }, (_, index) => index + 1);
+
   return (
-    <div>
+    <Wrapper>
       <Topbar />
-    </div>
+      <ScrollContainer>
+        {chartItems.map((id) => (
+          <ScrollArea key={id}>
+            <LyricsItem />
+          </ScrollArea>
+        ))}
+      </ScrollContainer>
+    </Wrapper>
   );
 };
 
 export default RecommendPage;
+
+const Wrapper = styled.div`
+  margin-top: 11.6rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ScrollContainer = styled.div`
+  overflow: auto;
+  scroll-snap-type: y mandatory;
+`;
+
+const ScrollArea = styled.div`
+  width: 100%;
+  height: 100%;
+  scroll-snap-align: start;
+  display: inline-block;
+`;
