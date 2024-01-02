168488f12753eb8e8c9760e623321ebdea7768f9
mod_siw <pvq_71@ewhain.net>
2024-01-03 02:17:08 +0900

    [feat] 추천 페이지 캐러셀 적용

---
+++ src/components/RecCarousel.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Slider from "react-slick";
+
+//components
+import LyricsItem from "./common/LyricsItem";
+
+const RecCarousel = () => {
+  const settings = {
+    arrows: false,
+    dots: false,
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    speed: 600,
+    vertical: true,
+    verticalSwiping: true,
+    swipeToSlide: true,
+  };
+  return (
+    <Wrapper>
+      <Slider {...settings} dotsClass="slick-dots-custom">
+        <LyricsItem />
+        <LyricsItem />
+        <LyricsItem />
+        <LyricsItem />
+        <LyricsItem />
+      </Slider>
+    </Wrapper>
+  );
+};
+
+export default RecCarousel;
+
+const Wrapper = styled.div``;
