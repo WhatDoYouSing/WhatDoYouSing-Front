f998cb6a9358e7c8257249710357a850a9fc3ce6
JiwonChoi0805 <won0805@ewhain.net>
2024-01-09 06:11:32 +0900

    댓글 및 답댓글 좋아요 제외 완료 + 스크랩 post 완료

---
+++ src/apis/archieve.js
@@ -0,0 +1,29 @@
+import { axiosInstance } from "../apis/http";
+
+// POST : 게시글 저장
+export const PostScrap = async (pk, scrapped) => {
+  try {
+    const response = await axiosInstance.post(`/posts/scrap/${pk}/`, {
+      pk: pk,
+      scrapped: scrapped,
+    });
+    console.log(response);
+    return Promise.resolve(response);
+  } catch (error) {
+    console.error("게시글 저장 실패", error.response);
+  }
+};
+
+// POST : 게시글 저장 취소
+export const PostCancelScrap = async (pk, scrapped) => {
+  try {
+    const response = await axiosInstance.post(`/posts/scrap/${pk}/`, {
+      pk: pk,
+      scrapped: scrapped,
+    });
+    console.log(response);
+    return Promise.resolve(response);
+  } catch (error) {
+    console.error("게시글 저장 취소 실패", error.response);
+  }
+};
--- src/components/TopTab.jsx
+++ src/components/TopTab.jsx
@@ -11,6 +11,8 @@ import MeatballSelect from "./DetailPage/MeatballSelect";
 
 import useClickOutside from "../hooks/useClickOutside";
 
+import { PostScrap, PostCancelScrap } from "../apis/archieve";
+
 const TopTab = ({
   share,
   setShare,
@@ -18,6 +20,7 @@ const TopTab = ({
   setDeletePost,
   reportPost,
   setReportPost,
+  postId,
 }) => {
   const navigate = useNavigate();
   const goBack = () => {
@@ -30,7 +33,14 @@ const TopTab = ({
 
   const [isBookmarked, setIsBookmarked] = useState(false);
   const handleBookmark = () => {
-    setIsBookmarked(!isBookmarked);
+    if (localStorage.getItem("token")) {
+      const Scrap = async (postId) => {
+        const response = await PostScrap(postId);
+        setIsBookmarked(!isBookmarked);
+        console.log(response);
+      };
+      Scrap(postId);
+    }
   };
 
   //외부 클릭시 닫힘
