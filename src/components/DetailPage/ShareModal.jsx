2ae74a235af8b027c9ecdf29548c1cba2d86683e
JiwonChoi0805 <won0805@ewhain.net>
2024-01-10 05:55:00 +0900

    공유하기 링크 및 로직 완료

--- src/components/DetailPage/ShareModal.jsx
+++ src/components/DetailPage/ShareModal.jsx
@@ -1,18 +1,29 @@
 import React, { useState, useRef } from "react";
 import styled from "styled-components";
-import { useNavigate } from "react-router";
+import { useNavigate, useLocation } from "react-router";
 
 import { ReactComponent as Save } from "../../images/save.svg";
 
 const ShareModal = ({ share, setShare, link }) => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleCopyClipBoard = async () => {
+    const textToCopy = `http://whatdoyousing.com${location.pathname}`;
+    try {
+      await navigator.clipboard.writeText(textToCopy);
+      alert("클립보드에 링크가 복사되었어요.");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Container>
       <Title>공유하기</Title>
       <UrlDiv>
-        <Url>https://youtu.be/nzwA9UpGl1k?si=jpFs03IVG5BUWCh5</Url>
-        <Btn>복사</Btn>
+        <Url>{`http://whatdoyousing.com${location.pathname}`}</Url>
+        <Btn onClick={handleCopyClipBoard}>복사</Btn>
       </UrlDiv>
       <ImgDiv onClick={() => navigate("/save")}>
         <SaveImg>이미지로 저장하기</SaveImg>
