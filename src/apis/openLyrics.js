ccbe3b05b4f6a0b2b52c576fa3fee70e3e128b12
mod_siw <pvq_71@ewhain.net>
2024-02-12 18:32:45 +0900

    [chore] proxy 연결 시도

--- src/apis/openLyrics.js
+++ src/apis/openLyrics.js
@@ -19,3 +19,39 @@ export const GetChartTracks = async (
     return Promise.reject(error);
   }
 };
+
+export const GetMusic = async () => {
+  try {
+    const response = await api.get(
+      `/api/chart.tracks.get?chart_name=hot&page=1&page_size=10&country=xw&f_has_lyrics=0&apikey=${process.env.REACT_APP_LYRICS_API_KEY}`
+    );
+    https: console.log(response.data);
+    return response.data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export const GetMusicSearch = async (q_artist, page_size, page) => {
+  try {
+    const response = await api.get(
+      `track.search?q_artist=${q_artist}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_LYRICS_API_KEY}`
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export const GetDetailLyrics = async (track_id, commontrack_id) => {
+  try {
+    const response = await api.get(
+      `track.lyrics.get?track_id=${track_id}&commontrack_id=${commontrack_id}&apikey=${process.env.REACT_APP_LYRICS_API_KEY}`
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
