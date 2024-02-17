32d24816ed0118569abdb29277cc45f065fa06ce
mod_siw <pvq_71@ewhain.net>
2024-02-18 00:44:55 +0900

    [feat] 사용 오픈 api 세팅

--- src/apis/openLyrics.js
+++ src/apis/openLyrics.js
@@ -12,7 +12,7 @@ export const GetChartTracks = async (
     const response = await api.get(
       `/api/chart.tracks.get?chart_name=${chart_name}&page=${page}&page_size=${page_size}&country=${country}&f_has_lyrics=${f_has_lyrics}`
     );
-    console.log(response);
+    console.log(response.data);
 
     return Promise.resolve(response.data);
   } catch (error) {
@@ -31,6 +31,29 @@ export const GetChartTracks = async (
 //     return Promise.reject(error);
 //   }
 // };
+export const GetArtistAlbums = async (artist_id, page_size, page) => {
+  try {
+    const response = await api.get(
+      `/api/artist.albums.get?artist_id=${artist_id}&s_release_date=desc&g_album_name=1&apikey=${process.env.REACT_APP_LYRICS_API_KEY}`
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export const GetAlbum = async (album_id, page_size, page) => {
+  try {
+    const response = await api.get(
+      `/api/album.tracks.get?album_id=${album_id}&page=1&page_size=10&apikey=${process.env.REACT_APP_LYRICS_API_KEY}`
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
 
 export const GetMusicSearch = async (q_artist, page_size, page) => {
   try {
@@ -49,7 +72,7 @@ export const GetDetailLyrics = async (track_id, commontrack_id) => {
     const response = await api.get(
       `/api/track.lyrics.get?track_id=${track_id}&commontrack_id=${commontrack_id}&apikey=${process.env.REACT_APP_LYRICS_API_KEY}`
     );
-    console.log(response);
+    console.log(response.data);
 
     return Promise.resolve(response.data);
   } catch (error) {
