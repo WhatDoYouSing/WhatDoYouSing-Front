0b11dd9b6ef13c803b045c33d1a99db891fe7b95
youtheyeon <yuseoyeon@ewhain.net>
2024-02-28 10:27:15 +0900

    [feat] open API 연결

--- src/apis/openLyrics.js
+++ src/apis/openLyrics.js
@@ -1,12 +1,28 @@
-import { api } from "../apis/http";
+import { spotifyApi, lyricApi } from "../apis/http";
 
 // GET : chart tracks
 export const GetChartTracks = async (title) => {
   try {
-    const response = await api.get(`search?q=${title}&type=track&limit=5`);
+    const response = await spotifyApi.get(
+      `search?q=${title}&type=track&limit=5`
+    );
     console.log(response.data.tracks.items);
 
     return Promise.resolve(response.data.tracks.items);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+// GET : chart tracks
+export const GetTrackLyric = async (id) => {
+  try {
+    const response = await lyricApi.get(
+      `https://zylalabs.com/api/1103/spotify+tracks+api/962/fetch+spotify+track+lyrics?id=${id}`
+    );
+    console.log(response.data);
+
+    return Promise.resolve(response.data.lyrics.lines);
   } catch (error) {
     return Promise.reject(error);
   }
