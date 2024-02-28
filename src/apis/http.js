6ef481301e17740cd18a8d4558da94fa97b01f0c
youtheyeon <yuseoyeon@ewhain.net>
2024-02-28 10:27:15 +0900

    [feat] open API 연결

--- src/apis/http.js
+++ src/apis/http.js
@@ -18,13 +18,16 @@ axiosInstance.defaults.headers.common["Authorization"] = token
 export const KAKAO_AUTH_URL = "http://whatdoyousing.com/accounts/kakao/";
 
 //오픈 API 관련
-const musicToken =
-  "BQAF8pSuZhqTkGrznSKTm310EUKmxd-RZzTHYS0Bsq21Cn9G46r6KGMgmNbVDd5LnJDuDOEcdnbD30vBYoQeHleqT9dnYORwV0mwETDzpBTvH6btwZoXCjhqbubHglHEVHbVG9KPEVC9KU5XJnW4Br99A-wBO1rGH8J51XyCe4QZy5RqSRXa9k1XAHhf13JA8NathA";
-
-export const api = axios.create({
+export const spotifyApi = axios.create({
   headers: {
-    Authorization: `Bearer ${musicToken}`,
+    Authorization: `Bearer ${process.env.REACT_APP_SPOTIFY_API_TOKEN}`,
   },
 });
 
-api.defaults.withCredentials = true;
+spotifyApi.defaults.withCredentials = true;
+
+export const lyricApi = axios.create({
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_LYRICS_API_KEY}`,
+  },
+});
