ff506b1b45aebb4e30a6804837e4c7667c39fa87
mod_siw <pvq_71@ewhain.net>
2024-03-30 15:43:43 +0900

    add : 저니맵 트레킹 코드 1차 삽입

--- src/pages/RecommendPage.jsx
+++ src/pages/RecommendPage.jsx
@@ -8,6 +8,21 @@ import Topbar from "../components/common/MainPage/Topbar";
 import { ReactComponent as Reload } from "../images/reload.svg";
 
 const RecommendPage = () => {
+  useEffect(() => {
+    (function (w, d, a) {
+      w.__beusablerumclient__ = {
+        load: function (src) {
+          var b = d.createElement("script");
+          b.src = src;
+          b.async = true;
+          b.type = "text/javascript";
+          d.getElementsByTagName("head")[0].appendChild(b);
+        },
+      };
+      w.__beusablerumclient__.load(a + "?url=" + encodeURIComponent(d.URL));
+    })(window, document, "//rum.beusable.net/load/b230311e131233u903");
+  }, []);
+
   return (
     <>
       <Topbar />
